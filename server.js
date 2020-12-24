7f0d708bd87a9f73bf0d1832cfe54b73ab8101ae
Brian Parker <btparker70@gmail.com>
2020-12-23 17:45:44 -0800

    added server.js info

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+  
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  // Use Handlebars to render the main index.html page with the plans in it.
+  app.get("/", function(req, res) {
+    res.send("Page is loading");
+
+  });
+
+  // Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
